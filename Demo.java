d32e87e3de2a650462a1783ef22f040356e6ee84
hotrongvi1101 <161850098+hotrongvi1101@users.noreply.github.com>
2024-03-30 21:43:16 +0700

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first program!");
+		System.out.println("My name's Vy");
+		System.out.println("My class's PIS");
+	}
+
+}
