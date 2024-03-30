e8032095b6234834e52fa630c7e3614ee49c4364
TrongVy <TrongVy@DESKTOP-3COPJCN>
2024-03-30 23:24:09 +0700

    New Comment

--- Demo.java
+++ Demo.java
@@ -4,8 +4,6 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first program!");
-		System.out.println("My name's Vy");
-		System.out.println("My class's PIS");
 	}
-
+//New Comment
 }
